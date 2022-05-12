23712ba68b1b33d185906f8189b629f1ccb30b15
Thomas Boos Pegler <tboospegler@gmail.com>
2022-05-12 15:07:59 -0400

    Exercicio 2

--- Fundamentos/Bloco-04-introducao-javascript-e-logica-de-programacao/Dia-01-primeiros-passos/scripts.js
+++ Fundamentos/Bloco-04-introducao-javascript-e-logica-de-programacao/Dia-01-primeiros-passos/scripts.js
@@ -1,10 +1,30 @@
-const myName = "Thomas";
-const birthCity = "Sao Paulo";
-let birthYear = 2000;
+// const myName = "Thomas";
+// const birthCity = "Sao Paulo";
+// let birthYear = 2000;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
 
 
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
 
+// console.log(typeof patientId)
+// console.log(typeof patientAge)
+
+
+const base = 5;
+const height = 8;
+const area = 5 * 8;
+const perimeter = 5 + 8 + 5 + 8;
+
+console.log(base);
+console.log(height);
+console.log(area);
+console.log(perimeter);
