bf759dff6b6e468fbe9f06763ed4b2c712b704b5
Thomas Boos Pegler <tboospegler@gmail.com>
2022-05-12 15:20:43 -0400

    Exercicio 3

--- Fundamentos/Bloco-04-introducao-javascript-e-logica-de-programacao/Dia-01-primeiros-passos/scripts.js
+++ Fundamentos/Bloco-04-introducao-javascript-e-logica-de-programacao/Dia-01-primeiros-passos/scripts.js
@@ -19,12 +19,28 @@
 // console.log(typeof patientAge)
 
 
-const base = 5;
-const height = 8;
-const area = 5 * 8;
-const perimeter = 5 + 8 + 5 + 8;
+// const base = 5;
+// const height = 8;
+// const area = 5 * 8;
+// const perimeter = 5 + 8 + 5 + 8;
 
-console.log(base);
-console.log(height);
-console.log(area);
-console.log(perimeter);
+// console.log(base);
+// console.log(height);
+// console.log(area);
+// console.log(perimeter);
+
+
+const notaCandidato = 70;
+
+if (notaCandidato >= 80 && notaCandidato <= 100) {
+  console.log("Parabéns, você foi aprovada(o)!")
+} 
+else if (notaCandidato < 80 && notaCandidato >= 60) {
+  console.log("Você esta na lista de espera");
+}
+else if (notaCandidato < 60) {
+  console.log("Você foi reprovada(o)");
+}
+else {
+  console.log("Digite um valor valido");
+}
