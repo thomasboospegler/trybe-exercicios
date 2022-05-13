a66f882685a313e78afd210753c0ae3d5b681a4f
Thomas Boos Pegler <tboospegler@gmail.com>
2022-05-13 14:18:04 -0400

    Exercicio 1

---
+++ Fundamentos/Bloco-04-introducao-javascript-e-logica-de-programacao/Dia-02-array-e-loop-for/exercicio1.js
@@ -0,0 +1,56 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+// // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// tasksList.unshift('Estudar');
+// console.log(tasksList);
+// // ['Estudar', 'Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+// // [ 'Tomar café', 'Reunião' ]
+// tasksList.shift();
+// console.log(tasksList);
+// // [ 'Reunião' ]
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let indexOfTask = tasksList.indexOf('Reunião');
+// console.log(indexOfTask);
+// // 1
+
+//Exercicio 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercicio 2
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exercicio 3
+menu.push('Contato');
+
+console.log(menu);
