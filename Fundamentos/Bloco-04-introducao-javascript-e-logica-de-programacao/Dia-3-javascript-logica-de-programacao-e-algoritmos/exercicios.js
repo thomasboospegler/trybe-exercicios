c5f9fbc7624f3c778b771de8e532b67a5ef99d2b
Thomas Boos Pegler <tboospegler@gmail.com>
2022-05-16 18:43:36 -0400

    Exercicios

---
+++ Fundamentos/Bloco-04-introducao-javascript-e-logica-de-programacao/Dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,60 @@
+//Exercicios 1
+let numero = 10;
+let fatorial = 1;
+
+for (let index = numero; index > 0; index -= 1) {
+  fatorial = fatorial * index;
+}
+console.log(fatorial);
+
+
+//Exercicio 2
+let word = 'tryber';
+let result = '';
+
+for (let index = word.length - 1; index >= 0; index -= 1) {
+  result += word[index]
+}
+console.log(result);
+
+
+//Exercicios 3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = '';
+let menor = '';
+
+for (let index = 0; index < array.length; index += 1) {
+  let tempResult = array[index];
+  if (tempResult.length > maior.length) {
+    maior = tempResult
+  }
+}
+console.log('A maior palavra deste array é:', maior);
+
+for (let index = 0; index < array.length; index += 1) {
+  let tempResult = array[index];
+  if (maior.length > tempResult.length) {
+    menor = tempResult
+  }
+}
+console.log('A menor palavra deste array é:', menor);
+
+
+//Exercicio 4
+let biggestPrime = 0;
+let primesList = [];
+
+for (n = 0; n <= 50; n += 1) {
+  let divider = 0;
+  for (x = 0; x <= n; x += 1) {
+    if (n % x == 0) {
+      divider += 1;
+    }
+  }
+  if (divider == 2) {
+    biggestPrime = n;
+    primesList.push(n);
+  }
+}
+console.log(biggestPrime);
+console.log(primesList);
