d7ff2449572684aac6e037e2c72ae36ef60e287c
Thomas Boos Pegler <tboospegler@gmail.com>
2022-06-09 14:28:38 -0400

    Aula - Objetos:ParteIII-Object.values - 7.2

---
+++ Fundamentos/Bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia-02-javascript-es6-fluxo-de-excecao-e-objetos/Objetos:ParteIII-Object.values/script.js
@@ -0,0 +1,64 @@
+// const coolestTvShow = {
+//   name: "BoJack Horseman",
+//   genre: "adult animation",
+//   createdBy: "Raphael Bob-Waksberg",
+//   favoriteCharacter: "Princess Carolyn",
+//   quote: "Princess Carolyn always lands on her feet.",
+//   seasons: 6,
+// };
+
+// for (const property in coolestTvShow) {
+//   console.log(coolestTvShow[property]);
+// }
+// // BoJack Horseman
+// // adult animation
+// // Raphael Bob-Waksberg
+// // Princess Carolyn
+// // Princess Carolyn always lands on her feet.
+// // 6
+
+
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+console.log(Object.values(coolestTvShow));
+// [
+//   'BoJack Horseman',
+//   'adult animation',
+//   'Raphael Bob-Waksberg',
+//   'Princess Carolyn',
+//   'Princess Carolyn always lands on her feet.'
+//    6
+// ]
+
+
+
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student)); //[ 'Muito Bom', 'Bom', 'Ótimo', 'Ótimo' ]
+
+// Com Object.values
+console.log(listSkillsValuesWithValues(student)); //[ 'Muito Bom', 'Bom', 'Ótimo', 'Ótimo' ]
