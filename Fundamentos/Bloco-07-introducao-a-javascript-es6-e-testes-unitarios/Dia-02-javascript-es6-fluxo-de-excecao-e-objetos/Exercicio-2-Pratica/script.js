8f00398d10fcd35cf169e7155a21f7c139468b07
Thomas Boos Pegler <tboospegler@gmail.com>
2022-06-09 16:13:10 -0400

    Exercicios - Exercicio-1/2/3-Pratica - 7.2

---
+++ Fundamentos/Bloco-07-introducao-a-javascript-es6-e-testes-unitarios/Dia-02-javascript-es6-fluxo-de-excecao-e-objetos/Exercicio-2-Pratica/script.js
@@ -0,0 +1,52 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, N: ${order.address.number}, AP: ${order.address.apartment}`);
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  order.name = 'Luiz Silva';
+  order.payment.total = 50;
+  const pizzas = Object.keys(order.order.pizza).join(', ')
+  console.log(`Olá ${order.name}, o total do seu pedido de ${pizzas} e ${order.order.drinks.coke.type} é R$ ${order.payment.total}`);
+}
+
+orderModifier(order);
