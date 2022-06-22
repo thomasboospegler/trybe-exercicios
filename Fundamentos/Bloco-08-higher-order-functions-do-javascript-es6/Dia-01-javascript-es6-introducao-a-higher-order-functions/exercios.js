57c44b25880bf46b215b8a808a2480f9b4f7766a
Thomas Boos Pegler <tboospegler@gmail.com>
2022-06-22 16:19:58 -0400

    Exercicios 8.1

---
+++ Fundamentos/Bloco-08-higher-order-functions-do-javascript-es6/Dia-01-javascript-es6-introducao-a-higher-order-functions/exercios.js
@@ -0,0 +1,52 @@
+// Exercicio 1
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'),
+    id2: func('Luiza Drumond'),
+    id3: func('Carla Paiva'),
+  }
+  return employees;
+};
+
+const generateEmail = (name) => {
+  const user = name.toLowerCase().split(' ').join('_');
+  return {Name: name, Email: `${user}@trybe.com`}
+}
+
+console.log(newEmployees(generateEmail));
+
+
+// Exerciio 2
+
+const sorteio = (num, func) => func(num)
+
+const checkNumber = (num) => {
+  const loteryNumber = Math.floor(Math.random() * 6);
+  return loteryNumber === num ? 'Parabéns você ganhou' : 'Tente novamente';
+  // if (loteryNumber === num) return 'Parabéns você ganhou'
+  // else return 'Tente novamente'
+}
+
+console.log(sorteio(2, checkNumber));
+
+
+
+// Exercicio 3
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const awnswersResult = (rigthAW, studentAW, func) => `Resultado final: ${func(rigthAW, studentAW)} pontos`;
+
+const checkAnswers = (rigthAW, studentAW) => {
+  let counter = 0;
+  for (let awnswerIndex in studentAW) {
+    studentAW[awnswerIndex] === rigthAW[awnswerIndex] ? counter += 1 : counter -= 0.5;
+    // if (studentAW[awnswerIndex] === rigthAW[awnswerIndex]) counter += 1;
+    // if (studentAW[awnswerIndex] !== rigthAW[awnswerIndex]) counter -= 0.5;
+  }
+  return counter;
+}
+
+console.log(awnswersResult(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
