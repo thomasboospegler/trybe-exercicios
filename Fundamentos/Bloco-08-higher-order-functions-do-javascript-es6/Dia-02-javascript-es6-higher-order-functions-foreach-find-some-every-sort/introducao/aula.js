3d59fc1f134e3066ed98eacc0dc7b5acecc51028
Thomas Boos Pegler <tboospegler@gmail.com>
2022-06-23 13:41:25 -0400

    Aula introdução 8.2

---
+++ Fundamentos/Bloco-08-higher-order-functions-do-javascript-es6/Dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/introducao/aula.js
@@ -0,0 +1,81 @@
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+
+function verifyGrades() {
+  for (let i = 0; i < students.length; i += 1) {
+    const student = students[i];
+    if (student.grade >= 60) {
+      student.approved = 'Aprovado';
+    } else {
+      student.approved = 'Recuperação';
+    }
+  }
+}
+// Mesma funçnao com forEach
+function verifyGrades() {
+  students.forEach((student, index) => {
+    if (student.grade >= 60) {
+      students[index].approved = 'Aprovado';
+    } else {
+      students[index].approved = 'Recuperação';
+    }
+  });
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
+
+// -------------------------------------------------------------
+
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+
+let firstMultiple5;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 5 === 0) {
+    firstMultiple5 = numbers[index];
+    break;
+  }
+}
+
+const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+console.log(firstMultipleOf5);
+// 50
+
+
+// -------------------------------------------------------------
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', indexOfTheArray);
+  console.log('Array percorrido:', theEntireArray);
+});
+
+// => ---------------
+//   Cada elemento do array: josé
+//   Index, posição do elemento: 0
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 50
+//   Index, posição do elemento: 1
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 0.25
+//   Index, posição do elemento: 2
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: { comida: 'Chocolate' }
+//   Index, posição do elemento: 3
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
