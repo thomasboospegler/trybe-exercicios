33ea07651cf013f4bc319ce9869ac7b95260dc64
Thomas Boos Pegler <tboospegler@gmail.com>
2022-11-30 09:50:51 -0400

    exercicios BE-6.1

---
+++ Back-End/Bloco-06-node.js-ORM-e-autenticação/Dia-01-ORM-interface-da-aplicação-com-o-banco-de-dados/exercicios/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const BooksController = require('./controllers/BooksController');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/books', BooksController.getAll);
+
+app.get('/books/:id', BooksController.getById);
+
+app.post('/books', BooksController.create);
+
+app.put('/books/:id', BooksController.update);
+
+app.delete('/books/:id', BooksController.remove);
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
