a4ff0b358c83959079844c20f263407e9b5aa5f8
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-25 16:42:35 -0400

    Exercicios aula CC-02.2

---
+++ Ciencia-da-computacao/Bloco-02-Padroes-de-Projeto/ Dia-02-Padroes-Iterator-Adapter-Strategy/aula/strategy.py
@@ -0,0 +1,73 @@
+class DebitoAutomatico:
+    @classmethod
+    def debitar(self, conta, valor, banco):
+        if banco == "itau":
+            # Codigo específico do Itaú (exemplo)
+            # connect_server_udp(itau_line)
+            # itau_line.check_system()
+            # itau_zig_zag(valor, 'Token 454')
+            print("Débito realizado pelo Itau")
+        elif banco == "Santander":
+            # Codigo específico do Santander (exemplo)
+            # connect_server_tcp(santander_line)
+            # santander_line.check_ping()
+            # metodo_106(valor)
+            print("Santander, Débito efetuado!")
+        elif banco == "Bradesco":
+            # Codigo específico do Bradesco (exemplo)
+            print("Sucesso!")
+        # ... + 150 bancos...
+        elif banco == "Caixa":
+            # Codigo específico da Caixa (exemplo)
+            print("Efetuado com sucesso, Caixa Agradece!")
+
+
+DebitoAutomatico.debitar(120, 123, "itau")
+DebitoAutomatico.debitar(110, 456, "Santander")
+DebitoAutomatico.debitar(120, 789, "Bradesco")
+
+
+from abc import ABC, abstractmethod
+
+
+class BancoStrategy(ABC):  # Interface
+    @classmethod
+    @abstractmethod
+    def debitar(cls):
+        raise NotImplementedError
+
+
+class ItauStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Itau (exemplo)
+        print("Débito realizado pelo Itau")
+
+
+class SantanderStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Santander (exemplo)
+        print("Santander, Débito efetuado!")
+
+
+class BradescoStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Bradesco (exemplo)
+        print("Sucesso!")
+
+# ... métodos para todos os bancos
+
+
+class Banco:
+    def __init__(self, banco_strategy):
+        self.__banco_strategy = banco_strategy
+
+    def debitar(self, conta, valor):
+        self.__banco_strategy.debitar(conta, valor)
+
+
+Banco(ItauStrategy).debitar(120, 123)
+Banco(SantanderStrategy).debitar(110, 456)
+Banco(BradescoStrategy).debitar(120, 789)
