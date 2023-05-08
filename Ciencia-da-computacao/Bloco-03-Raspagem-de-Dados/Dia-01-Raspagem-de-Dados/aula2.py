b0bd78b1755d9b126c0c89e483134eda63cdc7d3
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-08 08:30:29 -0400

    Exercicios aula CC-03.1

---
+++ Ciencia-da-computacao/Bloco-03-Raspagem-de-Dados/Dia-01-Raspagem-de-Dados/aula2.py
@@ -0,0 +1,40 @@
+import requests
+
+
+# À partir da décima requisição somos bloqueados de acessar o recurso
+# Código de status 429: Too Many Requests
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response.status_code)
+
+
+import requests
+import time
+
+
+# Coloca uma pausa de 6 segundos a cada requisição
+# Obs: este site de exemplo tem um rate limit de 10 requisições por minuto
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response)
+    time.sleep(6)
+
+
+import requests
+
+# Por 10 segundos não temos certeza se a requisição irá retornar
+response = requests.get("https://httpbin.org/delay/10")
+print(response)
+
+
+import requests
+
+
+try:
+    # recurso demora muito a responder
+    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.ReadTimeout:
+    # vamos fazer uma nova requisição
+    response = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    print(response.status_code)
---
+++ Ciencia-da-computacao/Bloco-03-Raspagem-de-Dados/Dia-01-Raspagem-de-Dados/aula6.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.catalogue
+documents = [
+    {
+        "title": "A Light in the Attic",
+    },
+    {
+        "title": "Tipping the Velvet",
+    },
+    {
+        "title": "Soumission",
+    },
+]
+db.books.insert_many(documents)
+client.close()  # fecha a conexão com o banco de dados
+
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.catalogue
+# busca um documento da coleção, sem filtros
+print(db.books.find_one())
+# busca utilizando filtros
+for book in db.books.find({"title": {"$regex": "t"}}):
+    print(book["title"])
+client.close()  # fecha a conexão com o banco de dados
+
+
+from pymongo import MongoClient
+
+
+with MongoClient() as client:
+    db = client.catalogue
+    for book in db.books.find({"title": {"$regex": "t"}}):
+        print(book["title"])
