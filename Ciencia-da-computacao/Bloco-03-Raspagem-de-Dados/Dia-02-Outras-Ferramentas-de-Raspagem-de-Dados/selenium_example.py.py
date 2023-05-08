cdf21a0267c84217e21eea9268cbf256ae943b13
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-08 16:48:14 -0400

    Exercicios aula CC-03.2

---
+++ Ciencia-da-computacao/Bloco-03-Raspagem-de-Dados/Dia-02-Outras-Ferramentas-de-Raspagem-de-Dados/selenium_example.py.py
@@ -0,0 +1,55 @@
+# importação do webdriver, que é o que possibilita a implementação para todos
+# os principais navegadores da web
+from time import sleep
+from selenium import webdriver
+
+# criação de uma instância de navegador utilizando o Firefox
+firefox = webdriver.Firefox()
+
+# requisições para essa instância criada utilizando o método `get`
+response = firefox.get("https://www.python.org/")
+
+
+# COM DOCKER
+# importação do webdriver, que é o que possibilita a implementação para todos
+# os principais navegadores da web
+from selenium import webdriver
+
+# Para usar o chrome ao invés do firefox trocamos FirefoxOptions por ChromeOptions
+# Todavia, caso esteja utilizando o docker, atente-se ao container sendo utilizado.
+options = webdriver.FirefoxOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--start-maximized')
+
+firefox = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
+
+# requisições para essa instância criada utilizando o método `get`
+response = firefox.get("https://www.python.org/")
+
+# espera 10 segundos
+sleep(10)
+# encerra o navegador, importante quando usamos containers
+firefox.quit()
+
+
+
+
+
+
+# from selenium import webdriver
+from selenium.webdriver.common.keys import Keys # Importa teclas comuns
+
+# firefox = webdriver.Firefox()
+
+response = firefox.get("https://www.google.com.br/")
+
+# pesquisa na instância aberta do navegador pela primeira ocorrência
+# do nome 'q'
+search_input = firefox.find_element("name", "q")
+
+# escreve selenium dentro do campo de pesquisa
+search_input.send_keys("selenium")
+
+# pressiona enter para realizar a busca
+search_input.send_keys(Keys.ENTER)
